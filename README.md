7acedb1483152a920457aeb26fc92754e182d62e
typicode <typicode@gmail.com>
2017-01-05 01:26:33 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# tlapse
+# tlapse [![npm](https://img.shields.io/npm/v/tlapse.svg)](https://github.com/typicode/tlapse) [![Standard - JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/)
+
+> Create a timelapse of your web development
+
+A tiny utility that mimics the timelapse mode of your camera/smartphone by taking a screenshot of your website periodically while you develop.
+
+To save disk space, consecutive same screenshots are automatically removed. Uses [pageres-cli](https://github.com/sindresorhus/pageres-cli) for creating screenshots.
+
+## Install
+
+```
+npm install -g tlapse
+```
+
+## Usage
+
+```sh
+tlapse -- localhost:3000
+```
+
+## Options
+
+To change interval and directory where screenshots are saved, use the following options
+
+```sh
+tlapse --every 5m --directory ./screens -- localhost:3000
+```
+
+You can also pass any [pageres-cli](https://github.com/sindresorhus/pageres-cli) options after `--`
+
+```sh
+tlapse -- localhost:3000 1366x768 --selector='.page-header'
+```
+
+## License
+
+MIT - [Typicode :cactus:](https://github.com/typicode)
---
+++ index.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+var fs = require('fs')
+var path = require('path')
+var debug = require('debug')('tlapse')
+var mkdirp = require('mkdirp')
+var execa = require('execa')
+var ms = require('ms')
+var dateFormat = require('dateformat')
+var pageresPath = path.join(__dirname, './node_modules/.bin/pageres')
+
+var yargs = require('yargs')
+  .usage('Usage: $0 [options] -- <pageres-cli-options>')
+  .option('every', {
+    alias: 'e',
+    describe: 'screenshots interval',
+    default: '1m',
+    type: 'string'
+  })
+  .option('directory', {
+    alias: 'd',
+    describe: 'screenshots directory',
+    default: './tlapse',
+    type: 'string'
+  })
+  .help()
+  .version()
+
+var argv = yargs.argv
+
+if (argv._.length === 0) {
+  return yargs.showHelp()
+} 
+
+if (argv.directory) {
+  mkdirp.sync(argv.directory)
+  process.chdir(argv.directory)
+}
+
+console.log('Screenshots directory:', argv.directory)
+console.log('Interval:', argv.every)
+
+function run () {
+  var pageresOptions = argv._.join(' ')
+  var cmd = [
+    pageresPath,
+    pageresOptions,
+    '--filename=\'' + Date.now() + '\''
+  ].join(' ')
+
+  debug(cmd)
+  const result = execa.shellSync(cmd)
+
+  if (result.error) {
+    console.log('Error')
+    console.log('stdout:', result.stdout)
+    console.log('stderr:', result.stderr)
+  } else {
+    var recentFiles = fs
+      .readdirSync(process.cwd())
+      .reverse()
+
+    var latestFile = recentFiles[0]
+    var previousFile = recentFiles[1]
+    var now = dateFormat(Date.now(), 'hh:mm:ss')
+
+    if (
+      latestFile &&
+      previousFile &&
+      fs.readFileSync(latestFile, 'utf-8') === fs.readFileSync(previousFile, 'utf-8')
+    ) {
+      console.log(now, '○ Duplicate screenshot, skipping')
+      return fs.unlinkSync(latestFile)
+    }
+
+    console.log(now, result.stdout.trim())
+  }
+}
+
+run()
+setInterval(run, ms(argv.every))
